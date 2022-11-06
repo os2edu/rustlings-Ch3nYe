f56f90a9c48a3fc3d957dca0fdf2c9d6338aebcd
Ch3nYe <sud0su@qq.com>
2022-11-06 21:29:57 +0800

    update

--- exercises/macros/macros2.rs
+++ exercises/macros/macros2.rs
@@ -1,14 +1,13 @@
 // macros2.rs
-// Execute `rustlings hint macros2` or use the `hint` watch subcommand for a hint.
+// Make me compile! Execute `rustlings hint macros2` for hints :)
 
-// I AM NOT DONE
-
-fn main() {
-    my_macro!();
-}
 
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
     };
+}
+
+fn main() {
+    my_macro!();
 }
